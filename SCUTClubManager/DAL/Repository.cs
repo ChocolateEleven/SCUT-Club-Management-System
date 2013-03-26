9b5b675fdd128d824e770d2cf35057d0c0a4be44
EternalWind <eternalwind@live.cn>
2013-03-26 13:29:47 +0800

    Changed member variables' naming matching the naming convension

--- SCUTClubManager/DAL/Repository.cs
+++ SCUTClubManager/DAL/Repository.cs
@@ -8,50 +8,50 @@ namespace SCUTClubManager.DAL
 {
     public class Repository<T> : IRepository<T> where T : class
     {
-        private SCUTClubContext mContext;
-        private DbSet<T> mTable;
+        private SCUTClubContext context;
+        private DbSet<T> table;
 
         public Repository()
         {
-            mContext = new SCUTClubContext();
-            mTable = mContext.Set<T>();
+            context = new SCUTClubContext();
+            table = context.Set<T>();
         }
 
         public Repository(SCUTClubContext context)
         {
-            mContext = context;
-            mTable = mContext.Set<T>();
+            this.context = context;
+            table = context.Set<T>();
         }
 
         public IEnumerable<T> ToList()
         {
-            return mTable;
+            return table;
         }
 
         public void Delete(T instance)
         {
-            if (mContext.Entry(instance).State == System.Data.EntityState.Detached)
+            if (context.Entry(instance).State == System.Data.EntityState.Detached)
             {
-                mTable.Attach(instance);
+                table.Attach(instance);
             }
 
-            mTable.Remove(instance);
+            table.Remove(instance);
         }
 
         public void Add(T instance)
         {
-            mTable.Add(instance);
+            table.Add(instance);
         }
 
         public void Update(T instance)
         {
-            mTable.Attach(instance);
-            mContext.Entry(instance).State = System.Data.EntityState.Modified;
+            table.Attach(instance);
+            context.Entry(instance).State = System.Data.EntityState.Modified;
         }
 
         public T Find(int id)
         {
-            return mTable.Find(id);
+            return table.Find(id);
         }
     }
 }
