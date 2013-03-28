cd273ec1a881b56fb33ffd9e880f993d0a0139e7
YIQING <chyq0524@qq.com>
2013-03-28 10:13:31 +0800

    更正若干正则表达式错误

--- SCUTClubManager/DAL/SCUTClubContext.cs
+++ SCUTClubManager/DAL/SCUTClubContext.cs
@@ -46,7 +46,7 @@ namespace SCUTClubManager.DAL
         public DbSet<UserPoll> UserPolls { get; set; }
         public DbSet<ApplicationRejectReason> ApplicationRejectReasons { get; set; }
 
-        //public SCUTClubContext() : base("DefaultConnection") { }
+        public SCUTClubContext() : base("DefaultConnection") { }
 
         protected override void OnModelCreating(DbModelBuilder model_builder)
         {
