fa8714f70b66d5d33ab69769aedfe7a082d1212d
EternalWind <eternalwind@live.cn>
2013-03-29 08:51:40 +0800

    Re-implemented the authentication and authorization mechanism

---
+++ SCUTClubManager/BusinessLogic/ScmMembershipProvider.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SCUTClubManager.DAL;
+using SCUTClubManager.Models;
+using System.Web.Security;
+
+namespace SCUTClubManager.BusinessLogic
+{
+    public class ScmMembershipProvider : MembershipProvider
+    {
+        private UnitOfWork context = new UnitOfWork();
+
+        public override bool ValidateUser(string user_name, string password)
+        {
+            if (string.IsNullOrEmpty(password.Trim()) || string.IsNullOrEmpty(user_name.Trim()))
+                return false;
+
+            string hash = FormsAuthentication.HashPasswordForStoringInConfigFile(password.Trim(), "md5");
+
+            return context.Users.ToList().Any(user => (user.UserName == user_name.Trim()) && (user.Password == hash));
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool ChangePassword(string user_name, string old_password, string new_password)
+        {
+            if (!ValidateUser(user_name, old_password) || string.IsNullOrEmpty(new_password.Trim()))
+                return false;
+
+            User user = context.Users.Find(user_name);
+            string hash = FormsAuthentication.HashPasswordForStoringInConfigFile(new_password.Trim(), "md5");
+
+            user.Password = hash;
+
+            context.SaveChanges();
+
+            return true;
+        }
+
+        public void CreateUser(string user_name, string password, string role_id)
+        {
+            var users = context.Users;
+            UserRole role = context.UserRoles.Find(role_id);
+            User user;
+
+            if (role != null && !users.ToList().Any(t => t.UserName == user_name))
+            {
+                if (role.Name == "Student")
+                {
+                    user = new Student
+                    {
+                        UserName = user_name,
+                        Password = FormsAuthentication.HashPasswordForStoringInConfigFile(password.Trim(), "md5"),
+                        Role = role
+                    };
+                }
+                else
+                {
+                user = new User
+                    {
+                        UserName = user_name,
+                        Password = FormsAuthentication.HashPasswordForStoringInConfigFile(password.Trim(), "md5"),
+                        Role = role
+                    };
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { return 0; }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { return 6; }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SCUTClubManager/BusinessLogic/ScmRoleProvider.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SCUTClubManager.Models;
+using SCUTClubManager.Models.View_Models;
+using System.Web.Security;
+using SCUTClubManager.DAL;
+
+namespace SCUTClubManager.BusinessLogic
+{
+    /**
+     *  用于处理已验证用户的访问权限。
+     */
+    public class ScmRoleProvider : RoleProvider
+    {
+        private UnitOfWork context = new UnitOfWork();
+
+        // 以下为社团内部权限管理用。
+
+        /**
+         *  查询当前用户是否属于某个社团的某个部门以及其职位。
+         *  @param club_id 要查询的社团。
+         *  @param branch_id 要查询的部门，为空则不查询该项。
+         *  @param possible_roles 要查询的职位。为空则不查询该项，为多个值则意味着满足任意一个值皆可。
+         *  @returns 查询结果。
+         */
+        public static bool HasMembershipIn(int club_id, int? branch_id = null, string[] possible_roles = null)
+        {
+            var user_identity = HttpContext.Current.User.Identity;
+            UnitOfWork context = new UnitOfWork();
+
+            if (user_identity.IsAuthenticated)
+            {
+                User user = context.Users.Find(user_identity.Name);
+
+                // 只有学生才能成为社团成员。
+                if (user is Student)
+                {
+                    Student student = user as Student;
+                    ClubMember membership = student.MemberShips.Single(t => t.ClubId == club_id);
+
+                    if (membership != null)
+                    {
+                        var all_roles = context.ClubRoles.ToList();
+
+                        if (!possible_roles.All(t => all_roles.Any(p => p.Name.Equals(t))))
+                        {
+                            throw new ArgumentException("One or more undefined club roles are given.");
+                        }
+
+                        if ((branch_id == null || membership.BranchId == branch_id) && (possible_roles == null ||
+                            possible_roles.Contains(membership.ClubRole.Name)))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // 以下为对用户全局权限的管理用。用于区分系统管理员和一般用户。
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string user_name)
+        {
+            User user = context.Users.Find(user_name);
+            List<string> roles = new List<string>();
+
+            if (user != null)
+            {
+                UserRole role = user.Role;
+
+                if (role != null)
+                {
+                    roles.Add(role.Name);
+                }
+            }
+
+            return roles.ToArray();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /**
+         *  查询给定用户是否为某个角色。建议使用Attribute语法而不是直接调用该方法。
+         *  @param user_name 要查询的用户的用户名。
+         *  @param role_name 要查询的角色名。
+         *  @returns 查询结果。
+         */
+        public override bool IsUserInRole(string user_name, string role_name)
+        {
+            if (!context.UserRoles.ToList().Any(t => t.Name.Equals(role_name)))
+            {
+                throw new ArgumentException("One or more undefined user roles are given.");
+            }
+
+            User user = context.Users.Find(user_name);
+
+            if (user != null && user.Role.Name.Equals(role_name))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
