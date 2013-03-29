50a65d7288ff8b3efe2f0b8078be403a439152bd
YIQING <chyq0524@qq.com>
2013-03-29 14:32:24 +0800

    messagecontroller

--- SCUTClubManager/DAL/SCUTClubContextInitializer.cs
+++ SCUTClubManager/DAL/SCUTClubContextInitializer.cs
@@ -65,13 +65,80 @@ namespace SCUTClubManager.DAL
                         Room = "C1-101",
                         Visibility = "s"
                     }
+                },
+
+                new Student { 
+                    UserName = "000000003",
+                    Password = "123456",
+                    RoleId = 1,
+                    FirstName = "三",
+                    LastName = "张",
+                    Gender = "M",
+                    Birthday = new DateTime(1990,01,01),
+                    Department = "生物学院",
+                    Major = "生物工程",
+                    Grade = "3",
+                    Degree = "b",
+                    PoliticalId = "t",
+                    ContactInfo = new ContactInfo
+                    {
+                        UserName = "000000003",
+                        QQ = "10086",
+                        Phone = "12580",
+                        Room = "C1-101",
+                        Visibility = "s"
+                    }
+                },
+
+                new Student { 
+                    UserName = "000000004",
+                    Password = "123456",
+                    RoleId = 1,
+                    FirstName = "四",
+                    LastName = "张",
+                    Gender = "M",
+                    Birthday = new DateTime(1990,01,01),
+                    Department = "生物学院",
+                    Major = "生物工程",
+                    Grade = "3",
+                    Degree = "b",
+                    PoliticalId = "t",
+                    ContactInfo = new ContactInfo
+                    {
+                        UserName = "000000004",
+                        QQ = "10086",
+                        Phone = "12580",
+                        Room = "C1-101",
+                        Visibility = "s"
+                    }
                 }
 
 
             };
 
-            context.RoleBases.Add(role);
             students.ForEach(s => context.Set<Student>().Add(s));
+
+
+            var messages = new List<Message>
+            {
+                new Message{
+                    SenderId = "000000004",
+                    ReceiverId = "000000002",
+                    Title = "test",
+                    ReadMark = true,
+                    Date = new DateTime(2009,09,09),
+                   Sender = students[3],
+                   Receiver = students[1],
+                   Content = new MessageContent{
+                       Content = "testtest"
+                   }
+                }
+            };
+
+            messages.ForEach(s => context.Messages.Add(s));
+
+
+            context.RoleBases.Add(role);
             context.SaveChanges();
         }
     }
