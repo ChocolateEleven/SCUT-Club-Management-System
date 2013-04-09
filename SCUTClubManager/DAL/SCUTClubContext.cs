4a246c93103014e08eda75b68dfe678498409668
EternalWind <eternalwind@live.cn>
2013-04-09 17:44:35 +0800

    More database relationship fixed
    1. Fixed the relationship between ClubInfoModificationApplication and
    ModificationBranch
    2. Since the UserName field is defined by user, I made the database no
    longer generate it automatically.
    3. Correct AssetBase with RoleBase which should be used originally

--- SCUTClubManager/DAL/SCUTClubContext.cs
+++ SCUTClubManager/DAL/SCUTClubContext.cs
@@ -131,9 +131,11 @@ namespace SCUTClubManager.DAL
                     m.ToTable("ClubRole");
                 });
 
-            model_builder.Entity<AssetBase>()
+            model_builder.Entity<RoleBase>()
                 .Property(p => p.Id)
                 .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+            model_builder.Entity<User>().Property(t => t.UserName).HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
 
             // 表之间的关系。
             model_builder.Entity<Student>().HasRequired(t => t.ContactInfo).WithRequiredPrincipal();
@@ -171,6 +173,8 @@ namespace SCUTClubManager.DAL
 
             model_builder.Entity<ClubRegisterApplication>().HasRequired(t => t.MajorInfo).WithRequiredDependent();
             model_builder.Entity<ClubRegisterApplication>().HasRequired(t => t.SubInfo).WithRequiredDependent();
+
+            model_builder.Entity<ClubInfoModificationApplication>().HasMany(t => t.ModificationBranches).WithOptional().WillCascadeOnDelete(true);
         }
     }
 }
