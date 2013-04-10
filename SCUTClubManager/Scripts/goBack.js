b6a62f2381b58888da485f15a84b08aacd4ae650
EternalWind <eternalwind@live.cn>
2013-04-10 20:49:26 +0800

    Renamed goBack.js to helpers.js to let it contain all the tiny js helpers
    I also added a handy function to implement placeholder texts for text
    inputs. And now the search bar is using it so you can see the effect
    once you merge you changes.

--- SCUTClubManager/Scripts/goBack.js
+++
@@ -1,3 +0,0 @@
-﻿function goBack() {
-    history.go(-1);
-}
---
+++ SCUTClubManager/Scripts/helpers.js
@@ -0,0 +1,13 @@
+﻿function goBack() {
+    history.go(-1);
+}
+
+function placeHolder(element, class_name) {
+    element.addClass(class_name);
+    element.click(function () {
+        if ($(this).hasClass(class_name)) {
+            $(this).removeClass(class_name);
+            $(this).val("");
+        }
+    });
+}
