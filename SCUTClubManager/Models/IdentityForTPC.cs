3e4f1a1f70787ffa7175f7bd73fe83117e226abe
EternalWind <eternalwind@live.cn>
2013-04-11 17:56:34 +0800

    Updated jQuery libs

---
+++ SCUTClubManager/Models/IdentityForTPC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCUTClubManager.Models
+{
+    public class IdentityForTPC
+    {
+        [Key]
+        [MaxLength(30)]
+        public string BaseName { get; set; }
+
+        [Required]
+        public int Identity { get; set; }
+    }
+}
