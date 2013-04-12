4140f37c30407f505aafcbf03192fd3c22fe2fef
EternalWind <eternalwind@live.cn>
2013-04-12 14:25:24 +0800

    Added 2 helpers to helper generate textboxes and texareas from client

--- SCUTClubManager/Scripts/helpers.js
+++ SCUTClubManager/Scripts/helpers.js
@@ -31,3 +31,56 @@ function placeHolder(element, class_name) {
         removePlaceHolder(element, class_name);
     });
 }
+
+function ValidationRules(required, required_msg, regex, regex_msg) {
+    this.required = required;
+    this.requiredMsg = required_msg == null ? "X" : required_msg;
+    this.regex = regex;
+    this.regexMsg = regex_msg == null ? "X" : required_msg;
+}
+
+function textboxFor(name, default_msg, class_name, options) {
+    var textbox = "<input type='text' name='" + name + "' value='" + default_msg + "' class='" + class_name + "' ";
+    var is_validation_required = false;
+
+    if (options != null) {
+        if (options.required) {
+            is_validation_required = true;
+            textbox += "data-val-required='" + options.required_msg + "' ";
+        }
+        if (options.regex != null) {
+            is_validation_required = true;
+            textbox += "data-val-regex='" + options.regex_msg + "' data-val-regex-pattern='" + options.regex + "' ";
+        }
+
+        if (is_validation_required) {
+            textbox += "data-val='true' ";
+        }
+    }
+
+    textbox += "/><span data-valmsg-for='" + name + "' data-valmsg-replace='true'></span>"
+    
+    return textbox;
+}
+
+function textareaFor(name, default_msg, class_name, options) {
+    var textarea = "<textarea name='" + name + "' class='" + class_name + "' ";
+    var is_validation_required = false;
+
+    if (options.required) {
+        is_validation_required = true;
+        textarea += "data-val-required='" + options.required_msg + "' ";
+    }
+    if (options.regex != null) {
+        is_validation_required = true;
+        textarea += "data-val-regex='" + options.regex_msg + "' data-val-regex-pattern='" + options.regex + "' ";
+    }
+
+    if (is_validation_required) {
+        textarea += "data-val='true' ";
+    }
+
+    textarea += ">" + default_msg + "</textarea><span data-valmsg-for='" + name + "' data-valmsg-replace='true'></span>"
+
+    return textarea;
+}
